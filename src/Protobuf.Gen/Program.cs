de344dfee61aa24ae974111700c9264dff03b877
假正经哥哥 <xuanye.wan@gmail.com>
2017-04-25 22:37:13 +0800

    初始化项目

---
+++ src/Protobuf.Gen/Program.cs
@@ -0,0 +1,39 @@
+﻿using Google.Protobuf;
+using Google.Protobuf.Compiler;
+using System;
+
+namespace DotBPE.ProtobufPlugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            var response = new CodeGeneratorResponse();
+            try
+            {
+                CodeGeneratorRequest request;
+                using (var inStream = Console.OpenStandardInput())
+                {
+                    request = CodeGeneratorRequest.Parser.ParseFrom(inStream);
+                }
+                ParseCode(request, response);
+            }
+            catch (Exception e)
+            {
+                response.Error += e.ToString();
+            }
+
+            using (var output = Console.OpenStandardOutput())
+            {
+                response.WriteTo(output);
+                output.Flush();
+
+            }
+        }
+        private static void ParseCode(CodeGeneratorRequest request, CodeGeneratorResponse response)
+        {
+
+        }
+    }
+}
